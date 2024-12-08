a38e2ee5fa93823f7df0105ccc01c69263f21962
mgjk88 <159707804+mgjk88@users.noreply.github.com>
2024-12-08 09:58:35 +0800

    Add operator dispatch function
    operate is a dispatch function that identifies
    the operator and applies the correct function to
    perform the operation.
    
    * Initialised variables to keep track of operator
      and operands.

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
+
+function operate(operator, num1, num2){ 
+    switch(operator){
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+
+}
+
+let firstNum = 0, operator = null, secondNum = 0;
